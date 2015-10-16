05f7a5b87c2385dfc52e818c2f36ca24ea951fe0
tonkotsuboy <t.kano.624@gmail.com>
2015-10-16 19:51:44 +0900

    初期ファイルのプッシュ

---
+++ TsudaRyass/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  TsudaRyass
+//
+//  Created by 鹿野壮 on 2015/10/16.
+//  Copyright (c) 2015年 鹿野壮. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        println("私達のこの口は");
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
